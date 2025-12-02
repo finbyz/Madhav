2f475c78f42024f06439e10a3d0ea558f236fd5d
Finbyz <info@finbyz.tech>
2025-12-02 07:20:52 +0000

    byp - changing js file for item groupwise report

--- madhav/madhav/report/item_groupwise__stock_madhav/item_groupwise__stock_madhav.js
+++ madhav/madhav/report/item_groupwise__stock_madhav/item_groupwise__stock_madhav.js
@@ -20,23 +20,11 @@ frappe.query_reports["Item Groupwise  Stock Madhav"] = {
 				}
 			}
 		},
-		// {
-		// 	"fieldname":"authorized",
-		// 	"label": __("Authorized"),
-		// 	"fieldtype": "Check",				
-		// },
-		// {
-		// 	"fieldname":"unauthorized",
-		// 	"label": __("Unauthorized"),
-		// 	"fieldtype": "Check",
-		// 	"default":1	
-		// },
 		{
 			"fieldname":"show_0_qty_inventory",
 			"label": __("Show 0 Qty Inventory"),
 			"fieldtype": "Check",				
 		},
-
 	],
 	"tree": true,
 	"name_field": "item_group",
@@ -70,69 +58,68 @@ frappe.query_reports["Item Groupwise  Stock Madhav"] = {
 			value = "<span style='color:red'>" + value + "</span>";
 		}
 
-
 		return value;
 	},
 }
 
-
-// function route_to_sbe(company,item_group){
-
-// 	frappe.set_route("query-report", "Stock Ledger Engineering",{
-// 		"company": company,
-// 		"item_group": item_group
-
-// 	});
-// }
-// function route_to_sle_item(company, item_name ){
-// 	// frappe.route_options = {
-// 	// 	item_group: me.frm.doc.item_group,
-// 	// 	company: me.frm.doc.company
-// 	// };
-// 	frappe.set_route("query-report", "Stock Ledger Engineering",{
-// 		"company": company,
-// 		"item_code": item_name
-// 	}); "item_code=" + item_name
-// }
-
+// Route to Stock Ledger for Item Group
 function route_to_sle(company, warehouse, item_group) {
-if (warehouse){
-	window.open(window.location.href.split('app')[0] + "app/query-report/Stock Ledger" + "/?" + "company=" + company + "&" + "warehouse="+ warehouse + "&" +"item_group="+item_group,"_blank")	
-}else{
-	window.open(window.location.href.split('app')[0] + "app/query-report/Stock Ledger" + "/?" + "company=" + company + "&" +  "item_group="+item_group,"_blank")	
-}
+	const base_url = window.location.origin;
+	let url = `${base_url}/app/query-report/Stock Ledger?company=${encodeURIComponent(company)}&item_group=${encodeURIComponent(item_group)}`;
+	
+	if (warehouse) {
+		url += `&warehouse=${encodeURIComponent(warehouse)}`;
+	}
+	
+	window.open(url, "_blank");
 }
 
+// Route to Stock Ledger for Item
 function route_to_sle_item(company, warehouse, item_name) {
-if (warehouse){
-	window.open(window.location.href.split('app')[0] + "app/query-report/Stock Ledger" + "/?" + "company="+company + "&" + "warehouse="+ warehouse + "&" + "item_code=" + item_name,"_blank")	
-}else{
-	window.open(window.location.href.split('app')[0] + "app/query-report/Stock Ledger" + "/?" + "company="+company + "&" + "item_code=" + item_name,"_blank")	
-}
+	const base_url = window.location.origin;
+	let url = `${base_url}/app/query-report/Stock Ledger?company=${encodeURIComponent(company)}&item_code=${encodeURIComponent(item_name)}`;
+	
+	if (warehouse) {
+		url += `&warehouse=${encodeURIComponent(warehouse)}`;
+	}
+	
+	window.open(url, "_blank");
 }
 
+// Route to Stock Balance for Item Group
 function route_to_stock_balance(company, warehouse, item_group) {
-if (warehouse){
-	window.open(window.location.href.split('app')[0] + "app/query-report/Stock Balance" + "/?" + "company=" + company + "&" +  "warehouse="+ warehouse + "&" + "show_warehouse_wise_balance=1" + "&" +"item_group="+item_group,"_blank")	
-}
-else{
-	window.open(window.location.href.split('app')[0] + "app/query-report/Stock Balance" + "/?" + "company=" + company + "&" +  "item_group="+item_group,"_blank")	
-}
+	const base_url = window.location.origin;
+	let url = `${base_url}/app/query-report/Stock Balance?company=${encodeURIComponent(company)}&show_warehouse_wise_balance=1&item_group=${encodeURIComponent(item_group)}`;
+	
+	if (warehouse) {
+		url += `&warehouse=${encodeURIComponent(warehouse)}`;
+	}
+	
+	window.open(url, "_blank");
 }
 
+// Route to Stock Balance for Item
 function route_to_stock_balance_item(company, warehouse, item_name) {
-if (warehouse){
-	window.open(window.location.href.split('app')[0] + "app/query-report/Stock Balance" + "/?" + "company="+company + "&" + "warehouse="+ warehouse + "&" + "show_warehouse_wise_balance=1" + "&" + "item_code=" + item_name,"_blank")	
-}
-else{
-	window.open(window.location.href.split('app')[0] + "app/query-report/Stock Balance" + "/?" + "company="+company + "&" + "item_code=" + item_name,"_blank")	
-}
+	const base_url = window.location.origin;
+	let url = `${base_url}/app/query-report/Stock Balance?company=${encodeURIComponent(company)}&show_warehouse_wise_balance=1&item_code=${encodeURIComponent(item_name)}`;
+	
+	if (warehouse) {
+		url += `&warehouse=${encodeURIComponent(warehouse)}`;
+	}
+	
+	window.open(url, "_blank");
 }
 
+// Route to Monthly Stock Summary for Item Group
 function route_to_monthly_stock_balance(company, item_group) {
-window.open(window.location.href.split('app')[0] + "app/query-report/Monthly Stock Summary" + "/?" + "company=" + company + "&" +  "item_group="+item_group,"_blank")	
+	const base_url = window.location.origin;
+	const url = `${base_url}/app/query-report/Monthly Stock Summary?company=${encodeURIComponent(company)}&item_group=${encodeURIComponent(item_group)}`;
+	window.open(url, "_blank");
 }
 
+// Route to Monthly Stock Summary for Item
 function route_to_monthly_stock_balance_item(company, item_name) {
-window.open(window.location.href.split('app')[0] + "app/query-report/Monthly Stock Summary" + "/?" + "company="+company + "&" + "item_code=" + item_name,"_blank")	
+	const base_url = window.location.origin;
+	const url = `${base_url}/app/query-report/Monthly Stock Summary?company=${encodeURIComponent(company)}&item_code=${encodeURIComponent(item_name)}`;
+	window.open(url, "_blank");
 }
