b7f554f546949099a5c22076b574a69d0a31afdf
Finbyz <info@finbyz.tech>
2026-05-16 06:40:28 +0000

    ps sales order py changes

--- madhav/doc_events/sales_order.py
+++ madhav/doc_events/sales_order.py
@@ -8,11 +8,11 @@ def calculate_qty_in_tonne(doc, method):
         if row.length_size and row.pieces:  
             total_length_in_meter += row.length_size * row.pieces
             item = frappe.get_doc("Item", row.item_code)            
-            weight_per_meter = float(item.weight_per_meter) or 0           
+            weight_per_meter = float(item.weight_per_meter) or 0       
             calculated_qty = (row.length_size * row.pieces * weight_per_meter) / 1000
-            
-            row.qty = calculated_qty
-            row.db_set('qty', calculated_qty, update_modified=False)
+            if not row.qty:
+                row.qty = calculated_qty
+                row.db_set('qty', calculated_qty, update_modified=False)
             
             total_qty += calculated_qty
     
